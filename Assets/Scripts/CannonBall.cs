f1e678a1f1431b05d7817aa6d624e5ef0ec5aae5
Bowen Tian <bowen.tian@mail.mcgill.ca>
2018-10-20 15:51:49 -0400

    Left Wall bouncing effect added

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class CannonBall : MonoBehaviour
 {
 	private float groundHeight = -7.711f;
-	private float screenLeftLimit = -20.163f;
+	private float leftWallBorder = -18f;
 	private float screenRightLimit = 20.06f;
 	private Vector2 velocity;
-	private float weight = 40;
+	private float weight = 35;
 	
 	// Use this for initialization
 	void Start ()
@@ -18,10 +18,16 @@ public class CannonBall : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.y < groundHeight || transform.position.x < screenLeftLimit 
-		                                        || transform.position.x > screenRightLimit)
+		if (transform.position.y < groundHeight || transform.position.x > screenRightLimit)
 		{
 			Destroy(gameObject);
+		}
+
+		float xSpeed = gameObject.GetComponent<Rigidbody>().velocity.x;
+		
+		if (transform.position.x < leftWallBorder && xSpeed < 0)
+		{
+			gameObject.GetComponent<Rigidbody>().velocity = new Vector2(-xSpeed,gameObject.GetComponent<Rigidbody>().velocity.y);
 		}
 
 		velocity += weight * Physics2D.gravity * Time.deltaTime;
