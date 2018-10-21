aa651acbe54179924a8d85858e9c970694689b9d
Bowen Tian <bowen.tian@mail.mcgill.ca>
2018-10-21 12:28:39 -0400

    Line Controller and turkey prefab

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+	public GameObject end;
+	private LineRenderer _lineRenderer;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_lineRenderer = GetComponent<LineRenderer>();
+		Vector3[] vector3s = new Vector3[]{transform.position,end.transform.position};
+		_lineRenderer.SetPositions(vector3s);
+	}
+}
